75eada8c01780148ea16df60bc2b8de29531d835
= <=>
2024-03-14 21:50:58 +0100

    Empezada calculadora de jubilaciones

---
+++ src/calculadoras/jubilacion.java
@@ -0,0 +1,25 @@
+package calculadoras;
+
+import java.util.Scanner;
+
+public class jubilacion {
+
+  public static void main(String[] args) {
+    
+    Scanner in = new Scanner(System.in);
+
+    int añosCot = in.nextInt();
+    int mesesCot = in.nextInt();
+
+    int meses = añosCot*12 +mesesCot;
+
+    int br = in.nextInt();
+
+    while (meses > 0) {
+      
+    }
+
+
+  }
+
+}
