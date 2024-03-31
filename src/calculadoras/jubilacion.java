c1e091be003faef1466f01459ea91236c13f35ec
UxioMendezPRO <uxio.mendez@hotmail.com>
2024-03-31 19:32:40 +0200

    Modificado jubilaciones

--- src/calculadoras/jubilacion.java
+++ src/calculadoras/jubilacion.java
@@ -16,18 +16,18 @@ public class jubilacion {
 
     double baseCotizacion = bccc * 300 / 350;
 
-    double porcentaje = 50;
+    double porcentaje = 0;
     // Suma porcentajes en funcion del los meses cotizados  
     if (meses >180) {
-      porcentaje += 180 *0.21;
+      porcentaje += 50;
       meses -= 180;
     }
     if (meses > 106) {
-      porcentaje += 106 * 0.19;
+      porcentaje += 106 * 0.21;
       meses -= 106;
     }
     if (meses > 146) {
-      porcentaje += 164 * 0.19;
+      porcentaje += 146 * 0.19;
       meses -= 146; 
     }
     if (meses > 0) {
